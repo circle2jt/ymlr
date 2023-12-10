52f0ba601c66263a30cfbe6aefbb7362b435a1d2
circle 2π <circle2jt@gmail.com>
2023-12-10 18:56:11 +0700

    feat: fn-debounce'touch, fn-throttle'touch
    - allow reused last agruments

---
+++ src/components/fn-throttle/fn-throttle-touch.ts
@@ -0,0 +1,23 @@
+import { ThrottleManager } from 'src/managers/throttle-manager'
+import { FNThrottleFlush } from './fn-throttle-flush'
+
+/** |**  fn-throttle'touch
+  touch throttle function. Reused last agruments (#Ref: lodash.throttle)
+  @order 6
+  @example
+  ```yaml
+    - fn-throttle'touch:
+        name: Delay to do something               # Throttle name to touch
+    # OR
+    - fn-throttle'touch: Delay to do something   # Throttle name to touch
+    # OR
+    - fn-throttle'touch:
+        - delay1
+        - delay2
+  ```
+*/
+export class FNThrottletouch extends FNThrottleFlush {
+  override exec() {
+    this.name?.forEach(name => { ThrottleManager.Instance.touch(name) })
+  }
+}
