708c9c7c906ce7eb1c546bdb13d1fcad83929c9a
circle2jt <circle2jt@gmail.com>
2024-11-15 13:45:27 +0700

    refactor(log): update log format
    Move context to the end line

--- src/libs/logger/console/index.ts
+++ src/libs/logger/console/index.ts
@@ -12,9 +12,9 @@ export const SPACE = chalk.gray('┆')
 
 export class ConsoleLogger extends Logger {
   private static readonly UpdateEvent = new EventEmitter().setMaxListeners(0)
-  private readonly updateContext: () => void
+  // private readonly updateContext: () => void
   private static MaxContextLength = 0
-  private tab = ''
+  // private tab = ''
 
   set context(ctx: string) {
     super.context = ctx
@@ -30,23 +30,24 @@ export class ConsoleLogger extends Logger {
 
   constructor(level: LoggerLevel | Level | undefined, context = '', errorStack: ErrorStack = {}, id = Logger.GenID(), indent = new Indent()) {
     super(level, context, errorStack, id, indent)
-    this.updateContext = () => {
-      this.tab = new Array(ConsoleLogger.MaxContextLength - this.context.length).fill('┄').join('')
-    }
-    ConsoleLogger.UpdateEvent.on('update-context', this.updateContext)
+    // this.updateContext = () => {
+    //   this.tab = new Array(ConsoleLogger.MaxContextLength - this.context.length).fill('┄').join('')
+    // }
+    // ConsoleLogger.UpdateEvent.on('update-context', this.updateContext)
     this.context = context
   }
 
   getPrefixMsg(level: any) {
-    return `${App.ThreadID} [${UtilityFunctionManager.Instance.format.date(new Date(), 'hh:mm:ss.ms')}] ${GetLoggerLevelName(level)} ${chalk.blue(this.context + ' ' + this.tab)} ${this.indent.indentString}`
+    return `${App.ThreadID} [${UtilityFunctionManager.Instance.format.date(new Date(), 'hh:mm:ss.ms')}] ${GetLoggerLevelName(level)} ${this.indent.indentString}%s \t ${chalk.gray(this.context)}`
+    // return `${App.ThreadID} [${UtilityFunctionManager.Instance.format.date(new Date(), 'hh:mm:ss.ms')}] ${GetLoggerLevelName(level)} ${chalk.blue(this.context + ' ' + this.tab)} ${this.indent.indentString}`
   }
 
   override info(msg: any, ...prms: any) {
     if (this.level?.is(LoggerLevel.info)) {
       if (typeof msg === 'string') {
-        console.info(this.getPrefixMsg(LoggerLevel.info) + ' ' + msg, ...prms)
+        console.info(this.getPrefixMsg(LoggerLevel.info), ' ' + msg, ...prms)
       } else {
-        console.info(this.getPrefixMsg(LoggerLevel.info) + ' %o', msg, ...prms)
+        console.info(this.getPrefixMsg(LoggerLevel.info) + ' %o', '', msg, ...prms)
       }
     }
     return this
@@ -55,9 +56,9 @@ export class ConsoleLogger extends Logger {
   override debug(msg: any, ...prms: any) {
     if (this.level?.is(LoggerLevel.debug)) {
       if (typeof msg === 'string') {
-        console.debug(this.getPrefixMsg(LoggerLevel.debug) + ' ' + msg, ...prms)
+        console.debug(this.getPrefixMsg(LoggerLevel.debug), ' ' + msg, ...prms)
       } else {
-        console.debug(this.getPrefixMsg(LoggerLevel.debug) + ' %o', msg, ...prms)
+        console.debug(this.getPrefixMsg(LoggerLevel.debug) + ' %o', '', msg, ...prms)
       }
     }
     return this
@@ -66,9 +67,9 @@ export class ConsoleLogger extends Logger {
   override warn(msg: any, ...prms: any) {
     if (this.level?.is(LoggerLevel.warn)) {
       if (typeof msg === 'string') {
-        console.warn(this.getPrefixMsg(LoggerLevel.warn) + ' ' + msg, ...prms)
+        console.warn(this.getPrefixMsg(LoggerLevel.warn), ' ' + msg, ...prms)
       } else {
-        console.warn(this.getPrefixMsg(LoggerLevel.warn) + ' %o', msg, ...prms)
+        console.warn(this.getPrefixMsg(LoggerLevel.warn) + ' %o', '', msg, ...prms)
       }
     }
     return this
@@ -77,9 +78,9 @@ export class ConsoleLogger extends Logger {
   override trace(msg: any, ...prms: any) {
     if (this.level?.is(LoggerLevel.trace)) {
       if (typeof msg === 'string') {
-        console.debug(this.getPrefixMsg(LoggerLevel.trace) + ' ' + msg, ...prms)
+        console.debug(this.getPrefixMsg(LoggerLevel.trace), ' ' + msg, ...prms)
       } else {
-        console.debug(this.getPrefixMsg(LoggerLevel.trace) + ' %o', msg, ...prms)
+        console.debug(this.getPrefixMsg(LoggerLevel.trace) + ' %o', '', msg, ...prms)
       }
     }
     return this
@@ -88,9 +89,9 @@ export class ConsoleLogger extends Logger {
   override error(msg: any, ...prms: any) {
     if (this.level?.is(LoggerLevel.error)) {
       if (typeof msg === 'string') {
-        console.error(this.getPrefixMsg(LoggerLevel.error) + ' ' + msg, ...prms)
+        console.error(this.getPrefixMsg(LoggerLevel.error), ' ' + msg, ...prms)
       } else {
-        console.error(this.getPrefixMsg(LoggerLevel.error) + ' %o', msg, ...prms)
+        console.error(this.getPrefixMsg(LoggerLevel.error) + ' %o', '', msg, ...prms)
       }
     }
     return this
@@ -99,9 +100,9 @@ export class ConsoleLogger extends Logger {
   override fatal(msg: any, ...prms: any) {
     if (this.level?.is(LoggerLevel.fatal)) {
       if (typeof msg === 'string') {
-        console.error(this.getPrefixMsg(LoggerLevel.fatal) + ' ' + msg, ...prms)
+        console.error(this.getPrefixMsg(LoggerLevel.fatal), ' ' + msg, ...prms)
       } else {
-        console.error(this.getPrefixMsg(LoggerLevel.fatal) + ' %o', msg, ...prms)
+        console.error(this.getPrefixMsg(LoggerLevel.fatal) + ' %o', '', msg, ...prms)
       }
     }
     if (this.errorStack) {
@@ -119,6 +120,6 @@ export class ConsoleLogger extends Logger {
   }
 
   override dispose() {
-    ConsoleLogger.UpdateEvent.off('update-context', this.updateContext)
+    // ConsoleLogger.UpdateEvent.off('update-context', this.updateContext)
   }
 }
