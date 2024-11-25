72648ea60220507ddb7532d6079297ec60bf2f64
circle2jt <circle2jt@gmail.com>
2024-11-25 16:27:50 +0700

    fix: merge InnerGroup into Group to prevent importing loop

--- src/components/group/group.ts
+++ src/components/group/group.ts
@@ -1,3 +1,4 @@
+import assert from 'assert'
 import { type AppEvent } from 'src/app-event'
 import ENVGlobal from 'src/env-global'
 import { GetLoggerLevel } from 'src/libs/logger/logger-level'
@@ -6,7 +7,6 @@ import { ElementProxy } from '../element-proxy'
 import { type Element, type ElementBaseProps, type ElementClass } from '../element.interface'
 import { Include } from '../include/include'
 import { type GroupItemProps, type GroupProps } from './group.props'
-import { InnerGroup } from './inner-group'
 
 /** |**  runs
   Group elements
@@ -403,5 +403,15 @@ export class Group<GP extends GroupProps, GIP extends GroupItemProps> implements
     }
     const elem = new ElemClass(props)
     return elem
+  }
+}
+
+class InnerGroup<GP extends GroupProps, GIP extends GroupItemProps> extends Group<GP, GIP> {
+  _parent!: Element
+
+  constructor(props?: GP & { _parent: Element }) {
+    assert(props?._parent)
+    super(props)
+    this._parent = props._parent
   }
 }
