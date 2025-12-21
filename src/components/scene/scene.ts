2f57d08a717e7a80f5fb4f8be684a26ef9806d52
circle2jt <circle2jt@gmail.com>
2025-12-21 11:16:11 +0700

    fix(scene): Support eval variable in runetime in "vars" in scene

--- src/components/scene/scene.ts
+++ src/components/scene/scene.ts
@@ -172,6 +172,7 @@ export class Scene extends Group<GroupProps, GroupItemProps> {
         await this.generateEncryptedFile(remoteFileProps, password)
       }
       const { name: _name, debug: _debug, vars: _vars, varsFiles: _varsFiles, ...groupProps } = remoteFileProps
+      await this.loadVars(_vars, [], [])
       const { name, debug, vars, varsFiles = [] } = await this.getVars({ name: _name, debug: _debug, vars: _vars, varsFiles: _varsFiles }, this.proxy)
       if (!this.proxy.debug && debug) this.proxy.setDebug(debug)
       if (this.name === undefined && name) this.name = name
